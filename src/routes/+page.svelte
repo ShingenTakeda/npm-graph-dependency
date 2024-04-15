60b706f63107a605a1cbcf00f8e669fc04c3e50b
ShingenTakeda <gustakeda@gmail.com>
2024-04-15 17:12:53 -0300

    Fazendo as estruturas para o DFS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,81 @@
     import fs from "node:fs"
     import a from "../package_copy.json"
 
+	class vertice
+	{
+		constructor()
+		{
+			this.nome = ""
+		}
+	}
+
+	class aresta
+	{
+		constructor(c, v, p, n)
+		{
+			this.comeco = c//vertice
+			this.dest = v//vertice destino
+			this.peso = p
+			this.nome = n
+		}
+	}
+
+	class grafo
+	{
+		constructor()
+		{
+			this.vertices = []
+			this.lista_adj = new Map();
+		}
+
+		addVertices(v)
+		{
+			if(v in this.vertices)
+			{
+				return
+			}
+
+			this.vertices.push(v)
+			this.lista_adj.set(v, [])
+		}
+
+		addAresta(v, u, peso = 0, nome = "")
+		{
+			if(!(v in this.vertices && u in this.vertices))
+			{
+				return
+			}
+			this.lista_adj.get(v).push(new aresta(v, u, peso, nome))
+		}
+
+		sao_adjacentes(v, u)
+		{
+			if(!(v in this.vertices && u in this.vertices))
+			{
+				return
+			}
+
+			this.lista_adj.get(v).forEach(element => {
+				if(element === u)
+				{
+					return true
+				}
+			})
+
+			return false
+		}
+
+		remover_aresta(v, a)
+		{
+			if(!(v in this.vertices && a in this.vertices.get(v)))
+			{
+				return
+			}
+
+			const indice = this.vertices.get(v).indexOf(a)
+			this.vertices.get(v).splice(index, 1)
+		}
+	}
     
     console.log(a)
 
